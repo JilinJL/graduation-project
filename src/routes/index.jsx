5dc022fe91cad4f16e713af265681b980c271e45
jilin <248789758@qq.com>
2024-03-15 08:21:56 +0800

    前端初始化

---
+++ src/routes/index.jsx
@@ -0,0 +1,14 @@
+export default function Index() {
+    return (
+        <p id="zero-state">
+            
+            <br />
+
+            情感分析系统
+
+            <div>
+                <input type="text" />
+            </div>
+        </p>
+    );
+}
